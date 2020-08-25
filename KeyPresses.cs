ba8316dc2334ec4d6de11725d74ef5d47dfa26b3
furkan-bayram-132 <furkan.engineer@gmail.com>
2020-08-25 18:58:27 +0300

    Setters turned into properties

--- KeyPresses.cs
+++ KeyPresses.cs
@@ -6,17 +6,20 @@ public class KeyPresses : MonoBehaviour
 {
     [SerializeField] List<KeyCode> desiredKeys;
 
-    private Dictionary<KeyCode, bool> keysBeingPressed = new Dictionary<KeyCode, bool>();
-    private Dictionary<KeyCode, bool> keysWasPressed = new Dictionary<KeyCode, bool>();
-    private Dictionary<KeyCode, bool> keysWasReleased = new Dictionary<KeyCode, bool>();
+    public Dictionary<KeyCode, bool> KeysBeingPressed { get; set; }
+    public Dictionary<KeyCode, bool> KeysWasPressed { get; set; }
+    public Dictionary<KeyCode, bool> KeysWasReleased { get; set; }
 
     void Awake()
     {
+        KeysBeingPressed = new Dictionary<KeyCode, bool>();
+        KeysWasPressed = new Dictionary<KeyCode, bool>();
+        KeysWasReleased = new Dictionary<KeyCode, bool>();
         foreach (KeyCode key in desiredKeys)
         {
-            keysBeingPressed.Add(key, false);
-            keysWasReleased.Add(key, false);
-            keysWasPressed.Add(key, false);
+            KeysBeingPressed.Add(key, false);
+            KeysWasReleased.Add(key, false);
+            KeysWasPressed.Add(key, false);
         }
     }
     void Update()
@@ -25,37 +28,25 @@ public class KeyPresses : MonoBehaviour
         SearchForKeysWasPressed();
         SearchForKeysWasReleased();
     }
-    public Dictionary<KeyCode, bool> GetKeysBeingPressed()
-    {
-        return keysBeingPressed;
-    }
-    public Dictionary<KeyCode, bool> GetKeysWasPressed()
-    {
-        return keysWasPressed;
-    }
-    public Dictionary<KeyCode, bool> GetKeysWasReleased()
-    {
-        return keysWasReleased;
-    }
     private void SearchForKeysBeingPressed()
     {
         foreach (KeyCode key in desiredKeys)
         {
-            keysBeingPressed[key] = Input.GetKey(key);
+            KeysBeingPressed[key] = Input.GetKey(key);
         }
     }
     private void SearchForKeysWasPressed()
     {
         foreach (KeyCode key in desiredKeys)
         {
-            keysWasPressed[key] = Input.GetKeyDown(key);
+            KeysWasPressed[key] = Input.GetKeyDown(key);
         }
     }
     private void SearchForKeysWasReleased()
     {
         foreach (KeyCode key in desiredKeys)
         {
-            keysWasReleased[key] = Input.GetKeyUp(key);
+            KeysWasReleased[key] = Input.GetKeyUp(key);
         }
     }
 
