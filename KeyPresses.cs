9cf7431135602554c4e8b8ac223368180959c766
furkan-bayram-132 <furkan.engineer@gmail.com>
2020-08-25 18:19:00 +0300

    All three key press situations are added

--- KeyPresses.cs
+++ KeyPresses.cs
@@ -14,22 +14,48 @@ public class KeyPresses : MonoBehaviour
     {
         foreach (KeyCode key in desiredKeys)
         {
-            keysBeingPressed[key] = false;
+            keysBeingPressed.Add(key, false);
+            keysWasReleased.Add(key, false);
+            keysWasPressed.Add(key, false);
         }
     }
     void Update()
     {
         SearchForKeysBeingPressed();
+        SearchForKeysWasPressed();
+        SearchForKeysWasReleased();
     }
     public Dictionary<KeyCode, bool> GetKeysBeingPressed()
     {
         return keysBeingPressed;
+    }
+    public Dictionary<KeyCode, bool> GetKeysWasPressed()
+    {
+        return keysWasPressed;
+    }
+    public Dictionary<KeyCode, bool> GetKeysWasReleased()
+    {
+        return keysWasReleased;
     }
     private void SearchForKeysBeingPressed()
     {
         foreach (KeyCode key in desiredKeys)
         {
             keysBeingPressed[key] = Input.GetKey(key);
+        }
+    }
+    private void SearchForKeysWasPressed()
+    {
+        foreach (KeyCode key in desiredKeys)
+        {
+            keysWasPressed[key] = Input.GetKeyDown(key);
+        }
+    }
+    private void SearchForKeysWasReleased()
+    {
+        foreach (KeyCode key in desiredKeys)
+        {
+            keysWasReleased[key] = Input.GetKeyUp(key);
         }
     }
 
