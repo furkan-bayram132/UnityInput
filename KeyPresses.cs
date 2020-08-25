47554732c57c19276610a8e23a506ccff7540700
furkan-bayram-132 <furkan.engineer@gmail.com>
2020-08-25 17:59:36 +0300

    KeysBeingPressed is done and the class KeyPresses inherits from MonoBehaviour from now on

--- KeyPresses.cs
+++ KeyPresses.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class KeyPresses : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] List<KeyCode> desiredKeys;
 
-    // Update is called once per frame
+    private Dictionary<KeyCode, bool> keysBeingPressed = new Dictionary<KeyCode, bool>();
+    private Dictionary<KeyCode, bool> keysWasPressed = new Dictionary<KeyCode, bool>();
+    private Dictionary<KeyCode, bool> keysWasReleased = new Dictionary<KeyCode, bool>();
+
+    void Awake()
+    {
+        foreach (KeyCode key in desiredKeys)
+        {
+            keysBeingPressed[key] = false;
+        }
+    }
     void Update()
     {
-        
+        SearchForKeysBeingPressed();
     }
+    public Dictionary<KeyCode, bool> GetKeysBeingPressed()
+    {
+        return keysBeingPressed;
+    }
+    private void SearchForKeysBeingPressed()
+    {
+        foreach (KeyCode key in desiredKeys)
+        {
+            keysBeingPressed[key] = Input.GetKey(key);
+        }
+    }
+
 }
